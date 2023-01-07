ed641c6415d0bea05f623aacb8e97bb226230f77
slows14tem <106790381+slows14tem@users.noreply.github.com>
2023-01-08 01:08:26 +0900

    2023-01-08 주석 추가

--- Chapter04/src/edu/pnu/JPAClientRemove.java
+++ Chapter04/src/edu/pnu/JPAClientRemove.java
@@ -22,6 +22,7 @@ public class JPAClientRemove {
 		try {
 			tx.begin();
 			//삭제할 게시글 조회
+			//영속성 컨텍스트에 삭제할 엔티티가 없다면 예외가 발생 -> 삭제할 엔티티를 검색해서 영속성 컨텍스트에 등록해야함
 			Board board1 = em.find(Board.class, 3L);
 			//find메소드로 board1에 이미 seq 3L이 저장되어있는데 다시 setSeq로 3L로 바꿔줄 필요없음
 			//게시글 삭제
